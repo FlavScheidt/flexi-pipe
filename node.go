f565ad3e248591ead343cd2d7363d1796c94d532
Flaviene <flaviene.scheidt@uni.lu>
2023-06-15 16:25:12 +0200

    Load into influx from the nodes themselves

---
+++ node.go
@@ -0,0 +1,44 @@
+package main
+
+import (  
+
+	// // "context"
+  	"fmt"
+ //  	// "os"
+  	"time"
+  	"log"
+ //  	"io"
+ //  	// "reflect"
+ //  	// "encoding/json"
+ //  	// "go-ndjson"
+
+  	// "github.com/bfontaine/jsons"
+  	// "github.com/influxdata/influxdb-client-go/v2/api/write"
+  	// influxdb2 "github.com/influxdata/influxdb-client-go/v2"
+
+
+    "golang.org/x/crypto/ssh"
+)
+
+func runNode(hostname string, config *ssh.ClientConfig, duration time.Duration) {
+
+    results := make(chan string, 10)
+    timeout := time.After(duration)
+
+    cmd := "cd "+PATH+" && "+GOPATH+"go run . -machine=node\n"
+    
+    go func(hostname string) {
+        results <- executeCmd(cmd, hostname, config)
+    }(hostname)
+    log.Println(hostname+":Command sent")
+    
+    // executeCmd(cmd, hostname, config)
+
+    select {
+        case res := <-results:
+            fmt.Print(res)
+        case <-timeout:
+            log.Println(hostname, ": Influx Load Timed out!")
+            return
+    }
+}
