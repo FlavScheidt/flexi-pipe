f6cd8a0b705e4a5621e0a5dba6113297392164e7
Flaviene <flaviene.scheidt@uni.lu>
2023-06-19 17:18:32 +0200

    Default execution time back to 900

--- main.go
+++ main.go
@@ -49,8 +49,8 @@ func main() {
 	machineFlag := flag.String("machine", "master", "Is this machine a master or a puppet? Deafult is master")
   	experimentType := flag.String("type", "unl", "Type of experiment. Default is unl")
 
-  	// runtime := flag.Duration("runtime", 900*time.Second, "Time for each test, counting from the start of gossipsub. Default is 900s (15 min)")
-  	runtime := flag.Duration("runtime", 100*time.Second, "Time for each test, counting from the start of gossipsub. Default is 900s (15 min)")
+  	runtime := flag.Duration("runtime", 900*time.Second, "Time for each test, counting from the start of gossipsub. Default is 900s (15 min)")
+  	// runtime := flag.Duration("runtime", 100*time.Second, "Time for each test, counting from the start of gossipsub. Default is 900s (15 min)")
 
 
     d := flag.String("d", "8", "")
