1bc7795a033b240d4af226780d4e0eeac50d3983
Flaviene <flaviene.scheidt@uni.lu>
2023-10-03 13:55:29 +0200

    Pythin script to submit transactions

---
+++ transactions.py
@@ -0,0 +1,44 @@
+from xrpl.clients import JsonRpcClient
+from xrpl.wallet import generate_faucet_wallet
+from xrpl.models.transactions import Payment
+from xrpl.utils import xrp_to_drops
+from xrpl.wallet import Wallet
+from xrpl.constants import CryptoAlgorithm
+
+
+#Connect
+JSON_RPC_URL = "http://localhost:5005/"
+client = JsonRpcClient(JSON_RPC_URL)
+
+#Generate wallets
+wallet1 = generate_faucet_wallet(client, debug=True)
+wallet2 = generate_faucet_wallet(client, debug=True)
+
+#Get account numbers
+account1 = wallet1.address
+account2 = wallet2.address
+
+#Prepare payment
+my_payment = xrpl.models.transactions.Payment(
+    account=account1,
+    amount=xrpl.utils.xrp_to_drops(22),
+    destination=account2,
+)
+print("Payment object:", my_payment)
+
+# Sign transaction
+signed_tx = xrpl.transaction.autofill_and_sign(
+        my_payment, client, wallet1)
+max_ledger = signed_tx.last_ledger_sequence
+tx_id = signed_tx.get_hash()
+
+print("Signed transaction:", signed_tx)
+print("Transaction cost:", xrpl.utils.drops_to_xrp(signed_tx.fee), "XRP")
+print("Transaction expires after ledger:", max_ledger)
+print("Identifying hash:", tx_id)
+
+#Submit
+try:
+    tx_response = xrpl.transaction.submit_and_wait(signed_tx, client)
+except xrpl.transaction.XRPLReliableSubmissionException as e:
+    exit(f"Submit failed: {e}")
